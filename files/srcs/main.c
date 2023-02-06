8bb4905021cbbce32cf3586cac8f95bd6bfafe3a
CRESPY Arthur <arthur@crespy.eu>
2023-02-06 14:25:14 +0100

    Main window definition, keycode sniffer and render frame function

--- files/srcs/main.c
+++ files/srcs/main.c
@@ -1,6 +1,69 @@
-#include <stdio.h>
+#include "./../mlx_linux/mlx.h"
+#include "./../mlx_linux/mlx_int.h"
 
-int main() {
-	printf("Hello, World!\n");
-	return 0;
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct	s_vars
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data  img;
+}				t_vars;
+
+void    mlx_free(t_vars vars)
+{
+	mlx_destroy_image(vars.mlx, vars.img.img);
+	mlx_destroy_window(vars.mlx, vars.mlx_win);
+	mlx_destroy_display(vars.mlx);
+	free(vars.mlx);
+}
+
+
+int key_press(int keycode, t_vars vars)
+{
+	printf("key pressed %d\n", keycode);
+	if (keycode == 65307)
+	{
+		mlx_free(vars);
+		exit(0);
+	}
+	return (0);
+}
+
+int render_frame(t_vars vars)
+{
+	mlx_put_image_to_window(vars.mlx, vars.mlx_win, vars.img.img, 0, 0);
+	return (0);
+}
+
+int	main(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.mlx_win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+	vars.img.img = mlx_new_image(vars.mlx, 1920, 1080);
+	vars.img.addr = mlx_get_data_addr(vars.img.img, &vars.img.bits_per_pixel, &vars.img.line_length, &vars.img.endian);
+
+	mlx_hook(vars.mlx_win, 2, 1L<<0, (int (*)(void)) key_press, &vars);
+	mlx_loop_hook(vars.mlx, (int (*)(void)) render_frame, &vars);
+	mlx_loop(vars.mlx);
+
+
+	mlx_put_image_to_window(vars.mlx, vars.mlx_win, vars.img.img, 0, 0);
+
+	//mlx_loop(vars.mlx);
+
+	//sleep(3);
+
+	mlx_free(vars);
+
+	return (0);
 }
