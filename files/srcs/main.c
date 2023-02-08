1e7e7ca7286a545235c1d9156183b33c5b8d7344
CRESPY Arthur <arthur@crespy.eu>
2023-02-08 21:35:46 +0100

    Minor changes in call and free mlx

--- files/srcs/main.c
+++ files/srcs/main.c
@@ -1,14 +1,16 @@
-#include "./../mlx_linux/mlx.h"
-#include "./../mlx_linux/mlx_int.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 14:30:49 by acrespy           #+#    #+#             */
+/*   Updated: 2023/02/08 17:29:50 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct	s_data
-{
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+#include "./../includes/so_long.h"
 
 void	mlx_free(t_vars *vars)
 {
@@ -16,6 +18,10 @@ void	mlx_free(t_vars *vars)
 		mlx_destroy_image(vars->mlx, vars->background.img);
 	if (vars->player.img)
 		mlx_destroy_image(vars->mlx, vars->player.img);
+	if (vars->coin.img)
+		mlx_destroy_image(vars->mlx, vars->coin.img);
+	if (vars->wall.img)
+		mlx_destroy_image(vars->mlx, vars->wall.img);
 	if (vars->mlx_win)
 		mlx_destroy_window(vars->mlx, vars->mlx_win);
 	if (vars->mlx_win)
@@ -65,41 +71,46 @@ int	key_press(int keycode, t_vars *vars)
 int	render_frame(t_vars *vars)
 {
 	mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->background.img, 0, 0);
+	mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->coin.img, vars->coin.x, vars->coin.y);
+	mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->wall.img, vars->wall.x, vars->wall.y);
 	mlx_put_image_to_window(vars->mlx, vars->mlx_win, vars->player.img, vars->player.x, vars->player.y);
-	usleep(100000/120);
+	usleep(100000 / 120);
 	return (0);
 }
 
-void mlx_initialize(t_vars *vars)
+void	mlx_initialize(t_vars *vars)
 {
 	vars->mlx = mlx_init();
 	if (!vars->mlx)
 		mlx_free(vars);
-
 	vars->mlx_win = mlx_new_window(vars->mlx, 800, 600, "So Long!");
 	if (!vars->mlx_win)
 		mlx_free(vars);
 }
 
-void mlx_initialize_img(t_vars *vars)
+void	mlx_initialize_img(t_vars *vars)
 {
 	vars->background.path = "./../assets/background.xpm";
 	vars->background.img = mlx_xpm_file_to_image(vars->mlx, vars->background.path, &vars->background.width, &vars->background.height);
 	if (!vars->background.img)
 		mlx_free(vars);
-
 	vars->player.x = 0;
 	vars->player.y = 0;
 	vars->player.path = "./../assets/sly_cooper.xpm";
 	vars->player.img = mlx_xpm_file_to_image(vars->mlx, vars->player.path, &vars->player.width, &vars->player.height);
 	if (!vars->player.img)
 		mlx_free(vars);
-
 	vars->coin.x = 64;
 	vars->coin.y = 64;
 	vars->coin.path = "./../assets/coin.xpm";
 	vars->coin.img = mlx_xpm_file_to_image(vars->mlx, vars->coin.path, &vars->coin.width, &vars->coin.height);
 	if (!vars->coin.img)
+		mlx_free(vars);
+	vars->wall.x = 128;
+	vars->wall.y = 128;
+	vars->wall.path = "./../assets/wall.xpm";
+	vars->wall.img = mlx_xpm_file_to_image(vars->mlx, vars->wall.path, &vars->wall.width, &vars->wall.height);
+	if (!vars->wall.img)
 		mlx_free(vars);
 }
 
@@ -107,23 +118,12 @@ int	main(void)
 {
 	t_vars	vars;
 
-	vars.mlx = mlx_init();
-	vars.mlx_win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
-	vars.img.img = mlx_new_image(vars.mlx, 1920, 1080);
-	vars.img.addr = mlx_get_data_addr(vars.img.img, &vars.img.bits_per_pixel, &vars.img.line_length, &vars.img.endian);
-
-	mlx_hook(vars.mlx_win, 2, 1L<<0, (int (*)(void)) key_press, &vars);
+	mlx_initialize(&vars);
+	mlx_initialize_img(&vars);
+	mlx_hook(vars.mlx_win, 2, 1L << 0, (int (*)(void)) key_press, &vars);
+	mlx_hook(vars.mlx_win, 17, 1L << 17, (int (*)(void)) mlx_free, &vars);
 	mlx_loop_hook(vars.mlx, (int (*)(void)) render_frame, &vars);
 	mlx_loop(vars.mlx);
-
-
-	mlx_put_image_to_window(vars.mlx, vars.mlx_win, vars.img.img, 0, 0);
-
-	//mlx_loop(vars.mlx);
-
-	//sleep(3);
-
-	mlx_free(vars);
-
+	mlx_free(&vars);
 	return (0);
 }
