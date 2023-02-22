ba65339c62add98ce8fa2a9144131fd7d99eea9f
CRESPY Arthur <arthur@crespy.eu>
2023-02-22 17:29:50 +0100

    Add check_collectible function to check if player stand on

---
+++ files/srcs/game_collectibles.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_collectibles.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 15:55:28 by acrespy           #+#    #+#             */
+/*   Updated: 2023/02/22 15:55:32 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../includes/so_long.h"
+
+void	check_collectible(t_vars *vars)
+{
+	int	i_col;
+	int	i_row;
+
+	i_col = vars->player.y / 64;
+	i_row = vars->player.x / 64;
+	if (vars->map.map[i_col][i_row] == 'C')
+		vars->coin.nb--;
+}
