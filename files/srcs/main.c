273735d97d23901ae01303496eaa213884b16005
CRESPY Arthur <arthur@crespy.eu>
2023-02-22 19:01:05 +0100

    Check args and add todo list

--- files/srcs/main.c
+++ files/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 14:30:49 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/08 17:29:50 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/22 18:51:17 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,25 @@ int	main(int argc, char **argv)
 {
 	t_vars	vars;
 
-	(void)argc;
+	check_args(argc, argv);
 	open_map(&vars, argv[1]);
 	mlx_initialize_win(&vars);
 	mlx_initialize_img(&vars);
 	mlx_loop_hook(vars.mlx, (int (*)(void)) render_frame, &vars);
 	mlx_hook(vars.mlx_win, 2, 1L << 0, (int (*)(void)) key_press, &vars);
-	mlx_hook(vars.mlx_win, 17, 1L << 17, (int (*)(void)) mlx_exit, &vars);
+	mlx_hook(vars.mlx_win, 17, 1L << 17, (int (*)(void)) mlx_hook_exit, &vars);
 	mlx_loop(vars.mlx);
 	mlx_free(&vars);
 	return (0);
 }
+
+// Need to check if the map is closed by walls (1)
+// Need to check the path of the map
+// Need to check the name of the map (extension)
+// Need to add a git clone of the MLX library in the Makefile
+// Need to execute the Makefile of the MLX library
+// ---------------------------- //
+// Maybe add a player animation
+// Maybe add enemies
+// Maybe add sprite animations
+// Maybe add the counter of the number of moves on the screen
