43734c38973b8ac1f065ba2f82f2255abfecf458
CRESPY Arthur <arthur@crespy.eu>
2023-02-22 18:56:56 +0100

    Free map if errors + separate check_map for Norminette + change check_map to handle this modifs.

--- files/srcs/map_check.c
+++ files/srcs/map_check.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_check.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 22:20:35 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/22 15:55:49 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/22 18:46:04 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,15 @@ void	check_map_shape(t_vars *vars)
 		while (vars->map.map[i_col][i_row])
 			i_row++;
 		if (i_row != ft_strlen(vars->map.map[0]))
-			ft_exit(vars, "Error: Map must be rectangular or square\n", 1);
+		{
+			free_map(vars);
+			ft_exit("Error: Map must be rectangular or square\n", 1);
+		}
 		i_col++;
 	}
 }
 
-void	check_map_content(t_vars *vars)
+void	check_map_letters(t_vars *vars)
 {
 	int	i_col;
 	int	i_row;
@@ -45,23 +48,24 @@ void	check_map_content(t_vars *vars)
 				&& vars->map.map[i_col][i_row] != 'P'
 				&& vars->map.map[i_col][i_row] != 'C'
 				&& vars->map.map[i_col][i_row] != 'E')
-				ft_exit(vars, "Error: Invalid character in map\n", 1);
+			{
+				free_map(vars);
+				ft_exit("Error: Invalid character in map\n", 1);
+			}
 			i_row++;
 		}
 		i_col++;
 	}
 }
 
-void	check_map_objects(t_vars *vars)
+void	check_map_player(t_vars *vars)
 {
 	int	i_col;
 	int	i_row;
 	int	p_count;
-	int	e_count;
 
 	i_col = 0;
 	p_count = 0;
-	e_count = 0;
 	while (vars->map.map[i_col])
 	{
 		i_row = 0;
@@ -69,21 +73,47 @@ void	check_map_objects(t_vars *vars)
 		{
 			if (vars->map.map[i_col][i_row] == 'P')
 				p_count++;
+			i_row++;
+		}
+		i_col++;
+	}
+	if (p_count != 1)
+	{
+		free_map(vars);
+		ft_exit("Error: Map must have one player\n", 1);
+	}
+}
+
+void	check_map_exit(t_vars *vars)
+{
+	int	i_col;
+	int	i_row;
+	int	e_count;
+
+	i_col = 0;
+	e_count = 0;
+	while (vars->map.map[i_col])
+	{
+		i_row = 0;
+		while (vars->map.map[i_col][i_row])
+		{
 			if (vars->map.map[i_col][i_row] == 'E')
 				e_count++;
 			i_row++;
 		}
 		i_col++;
 	}
-	if (p_count != 1)
-		ft_exit(vars, "Error: Map must have one player\n", 1);
 	if (e_count != 1)
-		ft_exit(vars, "Error: Map must have one exit\n", 1);
+	{
+		free_map(vars);
+		ft_exit("Error: Map must have one exit\n", 1);
+	}
 }
 
 void	check_map(t_vars *vars)
 {
 	check_map_shape(vars);
-	check_map_content(vars);
-	check_map_objects(vars);
+	check_map_letters(vars);
+	check_map_player(vars);
+	check_map_exit(vars);
 }
