fefa2d2f54ea7b00d3e59551d9611d6947a64ef9
CRESPY Arthur <arthur@crespy.eu>
2023-02-22 17:28:52 +0100

    Add WSAD moves + set the player as the last entity to show

--- files/srcs/mlx_run.c
+++ files/srcs/mlx_run.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mlx.c                                              :+:      :+:    :+:   */
+/*   mlx_run.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 22:23:44 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/21 22:26:21 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/22 15:56:09 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 
 int	key_press(int keycode, t_vars *vars)
 {
-	printf("key pressed %d\n", keycode);
 	if (keycode == 65307)
 		mlx_exit(vars);
-	if (keycode == 65361)
+	if (keycode == 65361 || keycode == 97)
 		move_left(vars);
-	if (keycode == 65362)
+	if (keycode == 65362 || keycode == 119)
 		move_up(vars);
-	if (keycode == 65363)
+	if (keycode == 65363 || keycode == 100)
 		move_right(vars);
-	if (keycode == 65364)
+	if (keycode == 65364 || keycode == 115)
 		move_down(vars);
 	return (0);
 }
@@ -44,13 +43,6 @@ int	render_frame(t_vars *vars)
 			if (vars->map.map[i_col][i_row] == '1')
 				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
 					vars->wall.img, i_row * 64, i_col * 64);
-			else if (vars->map.map[i_col][i_row] == 'P')
-			{
-				vars->player.x = i_row * 64;
-				vars->player.y = i_col * 64;
-				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
-					vars->player.img, i_row * 64, i_col * 64);
-			}
 			else if (vars->map.map[i_col][i_row] == 'C')
 				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
 					vars->coin.img, i_row * 64, i_col * 64);
@@ -61,6 +53,8 @@ int	render_frame(t_vars *vars)
 		}
 		i_col++;
 	}
+	mlx_put_image_to_window(vars->mlx, vars->mlx_win,
+		vars->player.img, vars->player.x, vars->player.y);
 	usleep(100000 / 120);
 	return (0);
 }
