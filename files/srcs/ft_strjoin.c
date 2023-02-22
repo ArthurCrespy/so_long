ec5a054476d24d8255f43e65e6a7f121ccdeaae2
CRESPY Arthur <arthur@crespy.eu>
2023-02-22 17:29:02 +0100

    Add ft_strjoin.c

---
+++ files/srcs/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 22:39:46 by acrespy           #+#    #+#             */
+/*   Updated: 2023/02/22 15:55:22 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i_s1;
+	int		i_s2;
+	char	*result;
+
+	i_s1 = 0;
+	i_s2 = 0;
+	result = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	if (!result)
+		return (NULL);
+	if (s1 && s1[i_s1] && s1[0] != '\0')
+	{
+		while (s1[i_s1])
+		{
+			result[i_s1] = s1[i_s1];
+			i_s1++;
+		}
+	}
+	while (s2 && s2[i_s2] && s2[0] != '\0')
+	{
+		result[i_s1 + i_s2] = s2[i_s2];
+		i_s2++;
+	}
+	result[i_s1 + i_s2] = '\0';
+	return (result);
+}
