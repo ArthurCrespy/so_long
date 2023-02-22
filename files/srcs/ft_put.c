38cf9874f3055dca13bb60e6eb440371fb7c204c
CRESPY Arthur <arthur@crespy.eu>
2023-02-22 17:29:14 +0100

    Add ft_put.c

---
+++ files/srcs/ft_put.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_put.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 22:45:59 by acrespy           #+#    #+#             */
+/*   Updated: 2023/02/22 15:55:12 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr(int n)
+{
+	long long	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	ft_putchar(nb % 10 + 48);
+}
+
+void	ft_putmoves(t_vars *vars)
+{
+	ft_putstr("Moves: ");
+	ft_putnbr(vars->player.nb);
+	ft_putchar('\n');
+}
