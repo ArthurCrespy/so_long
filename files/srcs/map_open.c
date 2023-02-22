a4f3b11be24228bb4c6e775bae9653a0efe312ce
CRESPY Arthur <arthur@crespy.eu>
2023-02-22 18:57:31 +0100

    Minor changes + free if errors

--- files/srcs/map_open.c
+++ files/srcs/map_open.c
@@ -3,39 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   map_open.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 22:18:49 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/22 15:55:54 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/22 18:48:47 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
-
-void check_path(t_vars *vars, char *path)
-{
-	int	fd;
-
-	fd = open(path, O_DIRECTORY);
-	if (fd >= 0)
-	{
-		ft_exit(vars, "Error: Path is a directory\n", 1);
-		exit(1);
-	}
-	fd = open(path, O_RDONLY);
-	if (fd < 0)
-	{
-		ft_exit(vars, "Error: File does not exist\n", 1);
-		exit(1);
-	}
-	close(fd);
-}
 
 void	save_map(t_vars *vars, char *map)
 {
 	vars->map.map = ft_split(map, '\n');
 	vars->map.height = ft_strlen(map) / ft_strlen(vars->map.map[0]) * 64;
 	vars->map.width = ft_strlen(vars->map.map[0]) * 64;
+}
+
+void	check_path(char *path)
+{
+	int	fd;
+
+	fd = open(path, O_DIRECTORY);
+	if (fd >= 0)
+		ft_exit("Error: Path is a directory\n", 1);
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		ft_exit("Error: File does not exist\n", 1);
+	close(fd);
 }
 
 void	read_map(t_vars *vars, char *path)
@@ -45,12 +39,12 @@ void	read_map(t_vars *vars, char *path)
 	char	*gnl;
 	char	*join;
 
-	join = calloc(sizeof (char), 1);
+	join = ft_calloc(sizeof (char), 1);
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
 	{
 		free(join);
-		ft_exit(vars, "Error: Open failed\n", 1);
+		ft_exit("Error: Open failed\n", 1);
 	}
 	while (1)
 	{
@@ -67,10 +61,10 @@ void	read_map(t_vars *vars, char *path)
 	close(fd);
 }
 
-void    count_objects(t_vars *vars)
+void	count_objects(t_vars *vars)
 {
-	int i_col;
-	int i_row;
+	int	i_col;
+	int	i_row;
 
 	i_col = 0;
 	vars->coin.nb = 0;
@@ -89,7 +83,7 @@ void    count_objects(t_vars *vars)
 
 void	open_map(t_vars *vars, char *path)
 {
-	check_path(vars, path);
+	check_path(path);
 	read_map(vars, path);
 	check_map(vars);
 	count_objects(vars);
