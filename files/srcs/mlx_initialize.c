8e467e4f1de267a8c3a77f4d3a9300e3abd95806
CRESPY Arthur <arthur@crespy.eu>
2023-02-23 13:27:22 +0100

    Initialize new textures

--- files/srcs/mlx_initialize.c
+++ files/srcs/mlx_initialize.c
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 22:11:48 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/22 21:16:24 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/23 12:56:10 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,28 @@ void	mlx_initialize_win(t_vars *vars)
 		mlx_error(vars);
 }
 
-void	mlx_initialize_img(t_vars *vars)
+void	mlx_initialize_player_img(t_vars *vars)
+{
+	vars->player.path = "./../assets/player.xpm";
+	vars->player.img = mlx_xpm_file_to_image(vars->mlx, vars->player.path,
+			&vars->player.width, &vars->player.height);
+	if (!vars->player.img)
+		mlx_error(vars);
+	vars->player.path1 = "./../assets/player1.xpm";
+	vars->player.img1 = mlx_xpm_file_to_image(vars->mlx, vars->player.path1,
+			&vars->player.width, &vars->player.height);
+	if (!vars->player.img1)
+		mlx_error(vars);
+}
+
+void	mlx_initialize_player(t_vars *vars)
 {
 	int	i_row;
 	int	i_col;
 
 	i_row = 0;
-	vars->background.path = "./../assets/background.xpm";
-	vars->background.img = mlx_xpm_file_to_image(vars->mlx,
-			vars->background.path,
-			&vars->background.width, &vars->background.height);
-	if (!vars->background.img)
-		mlx_error(vars);
-	vars->wall.path = "./../assets/wall.xpm";
-	vars->wall.img = mlx_xpm_file_to_image(vars->mlx, vars->wall.path,
-			&vars->wall.width, &vars->wall.height);
-	if (!vars->wall.img)
-		mlx_error(vars);
 	vars->player.nb = 0;
+	vars->player.nb1 = 1;
 	while (vars->map.map[i_row])
 	{
 		i_col = 0;
@@ -64,19 +68,46 @@ void	mlx_initialize_img(t_vars *vars)
 		}
 		i_row++;
 	}
-	vars->player.path = "./../assets/sly_cooper.xpm";
-	vars->player.img = mlx_xpm_file_to_image(vars->mlx, vars->player.path,
-			&vars->player.width, &vars->player.height);
-	if (!vars->player.img)
+	mlx_initialize_player_img(vars);
+}
+
+void	mlx_initialize_exit(t_vars *vars)
+{
+	vars->exit.path = "./../assets/exit.xpm";
+	vars->exit.img = mlx_xpm_file_to_image(vars->mlx, vars->exit.path,
+			&vars->exit.width, &vars->exit.height);
+	if (!vars->exit.img)
+		mlx_error(vars);
+	vars->exit.path1 = "./../assets/exit1.xpm";
+	vars->exit.img1 = mlx_xpm_file_to_image(vars->mlx, vars->exit.path1,
+			&vars->exit.width, &vars->exit.height);
+	if (!vars->exit.img1)
+		mlx_error(vars);
+}
+
+void	mlx_initialize_img(t_vars *vars)
+{
+	mlx_initialize_player(vars);
+	mlx_initialize_exit(vars);
+	vars->background.path = "./../assets/background.xpm";
+	vars->background.img = mlx_xpm_file_to_image(vars->mlx,
+			vars->background.path,
+			&vars->background.width, &vars->background.height);
+	if (!vars->background.img)
+		mlx_error(vars);
+	vars->wall.path = "./../assets/wall.xpm";
+	vars->wall.img = mlx_xpm_file_to_image(vars->mlx, vars->wall.path,
+			&vars->wall.width, &vars->wall.height);
+	if (!vars->wall.img)
 		mlx_error(vars);
 	vars->coin.path = "./../assets/coin.xpm";
 	vars->coin.img = mlx_xpm_file_to_image(vars->mlx, vars->coin.path,
 			&vars->coin.width, &vars->coin.height);
 	if (!vars->coin.img)
 		mlx_error(vars);
-	vars->exit.path = "./../assets/exit.xpm";
-	vars->exit.img = mlx_xpm_file_to_image(vars->mlx, vars->exit.path,
-			&vars->exit.width, &vars->exit.height);
-	if (!vars->exit.img)
+	vars->hole.path = "./../assets/hole.xpm";
+	vars->hole.img = mlx_xpm_file_to_image(vars->mlx, vars->hole.path,
+			&vars->hole.width, &vars->hole.height);
+	if (!vars->hole.img)
 		mlx_error(vars);
 }
