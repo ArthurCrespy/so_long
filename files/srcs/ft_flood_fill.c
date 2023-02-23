9da740b68165a7fed93f52ee30b87a85688af872
CRESPY Arthur <arthur@crespy.eu>
2023-02-23 13:20:48 +0100

    Because of Norminette split ft_flood_fill in two separate tests: coin/exit

--- files/srcs/ft_flood_fill.c
+++ files/srcs/ft_flood_fill.c
@@ -6,29 +6,48 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 21:13:38 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/22 21:13:39 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/23 00:14:26 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//
-// Created by acrespy on 2/22/23.
-//
+#include "../includes/so_long.h"
 
-int ft_flood_fill(char **map, char c, int x, int y, int width, int height)
+int	ft_flood_fill_coin(t_vars *vars, char **map, int x, int y)
 {
-	int nb;
+	int	nb;
 
 	nb = 0;
-	if (x < 0 || x >= width || y < 0 || y >= height)
+	if (x < 0 || x >= vars->map.width
+		|| y < 0 || y >= vars->map.height)
 		return (0);
-	if (map[y][x] == '1')
+	if (map[y][x] == '1' || map[y][x] == 'X')
 		return (0);
-	if (map[y][x] == c)
+	if (map[y][x] == 'C')
 		nb++;
 	map[y][x] = '1';
-	nb += ft_flood_fill(map, c, x + 1, y, width, height);
-	nb += ft_flood_fill(map, c, x - 1, y, width, height);
-	nb += ft_flood_fill(map, c, x, y + 1, width, height);
-	nb += ft_flood_fill(map, c, x, y - 1, width, height);
+	nb += ft_flood_fill_coin(vars, map, x + 1, y);
+	nb += ft_flood_fill_coin(vars, map, x - 1, y);
+	nb += ft_flood_fill_coin(vars, map, x, y + 1);
+	nb += ft_flood_fill_coin(vars, map, x, y - 1);
+	return (nb);
+}
+
+int	ft_flood_fill_exit(t_vars *vars, char **map, int x, int y)
+{
+	int	nb;
+
+	nb = 0;
+	if (x < 0 || x >= vars->map.width
+		|| y < 0 || y >= vars->map.height)
+		return (0);
+	if (map[y][x] == '1' || map[y][x] == 'X')
+		return (0);
+	if (map[y][x] == 'E')
+		nb++;
+	map[y][x] = '1';
+	nb += ft_flood_fill_exit(vars, map, x + 1, y);
+	nb += ft_flood_fill_exit(vars, map, x - 1, y);
+	nb += ft_flood_fill_exit(vars, map, x, y + 1);
+	nb += ft_flood_fill_exit(vars, map, x, y - 1);
 	return (nb);
 }
