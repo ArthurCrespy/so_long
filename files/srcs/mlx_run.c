2c1376c48b2fdf59485754057f2bc8abc7866e1e
CRESPY Arthur <arthur@crespy.eu>
2023-02-23 13:27:45 +0100

    Split render_frame() and render new textures

--- files/srcs/mlx_run.c
+++ files/srcs/mlx_run.c
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 22:23:44 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/22 21:16:22 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/23 12:58:40 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,41 @@ int	key_press(int keycode, t_vars *vars)
 	return (0);
 }
 
-int	render_frame(t_vars *vars)
+void	render_collectibles(t_vars *vars)
 {
 	int	i_col;
 	int	i_row;
 
 	i_col = 0;
-	mlx_put_image_to_window(vars->mlx, vars->mlx_win,
-		vars->background.img, 0, 0);
+	while (vars->map.map[i_col])
+	{
+		i_row = 0;
+		while (vars->map.map[i_col][i_row])
+		{
+			if (vars->map.map[i_col][i_row] == 'X')
+				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
+					vars->hole.img, i_row * 64, i_col * 64);
+			else if (vars->map.map[i_col][i_row] == 'C')
+				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
+					vars->coin.img, i_row * 64, i_col * 64);
+			else if (vars->coin.nb != 0 && vars->map.map[i_col][i_row] == 'E')
+				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
+					vars->exit.img, i_row * 64, i_col * 64);
+			else if (vars->coin.nb == 0 && vars->map.map[i_col][i_row] == 'E')
+				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
+					vars->exit.img1, i_row * 64, i_col * 64);
+			i_row++;
+		}
+		i_col++;
+	}
+}
+
+void	render_walls(t_vars *vars)
+{
+	int	i_col;
+	int	i_row;
+
+	i_col = 0;
 	while (vars->map.map[i_col])
 	{
 		i_row = 0;
@@ -43,18 +70,35 @@ int	render_frame(t_vars *vars)
 			if (vars->map.map[i_col][i_row] == '1')
 				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
 					vars->wall.img, i_row * 64, i_col * 64);
-			else if (vars->map.map[i_col][i_row] == 'C')
-				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
-					vars->coin.img, i_row * 64, i_col * 64);
-			else if (vars->map.map[i_col][i_row] == 'E')
-				mlx_put_image_to_window(vars->mlx, vars->mlx_win,
-					vars->exit.img, i_row * 64, i_col * 64);
 			i_row++;
 		}
 		i_col++;
 	}
+}
+
+void	render_player(t_vars *vars)
+{
+	if (vars->player.nb1 == -1)
+		mlx_put_image_to_window(vars->mlx, vars->mlx_win,
+			vars->player.img, vars->player.x, vars->player.y);
+	if (vars->player.nb1 == 1)
+		mlx_put_image_to_window(vars->mlx, vars->mlx_win,
+			vars->player.img1, vars->player.x, vars->player.y);
+}
+
+int	render_frame(t_vars *vars)
+{
+	char	*count;
+
+	count = ft_itoa(vars->player.nb);
 	mlx_put_image_to_window(vars->mlx, vars->mlx_win,
-		vars->player.img, vars->player.x, vars->player.y);
-	usleep(100000 / 120);
+		vars->background.img, 0, 0);
+	render_walls(vars);
+	render_collectibles(vars);
+	render_player(vars);
+	mlx_string_put(vars->mlx, vars->mlx_win, 10, 10, 0xFFFFFF, "NB of moves:");
+	mlx_string_put(vars->mlx, vars->mlx_win, 150, 10, 0xFFFFFF, count);
+	free(count);
+	usleep(100000 / 60);
 	return (0);
 }
