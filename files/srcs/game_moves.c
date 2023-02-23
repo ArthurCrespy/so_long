92f68fea6b3e6449acdd76fc8356eeb987e96d4d
CRESPY Arthur <arthur@crespy.eu>
2023-02-23 13:22:45 +0100

    Reorganize move_*() functions

--- files/srcs/game_moves.c
+++ files/srcs/game_moves.c
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 22:16:34 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/22 21:15:30 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/23 12:56:59 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,44 +30,10 @@ int	check_collision(t_vars *vars, int direction)
 	return (0);
 }
 
-void	move_right(t_vars *vars)
-{
-	if (vars->player.x + 64 <= vars->screen.width - vars->player.width
-		&& !check_collision(vars, 4))
-	{
-		if (check_stand_exit(vars))
-			vars->map.map[vars->player.y / 64][vars->player.x / 64] = 'E';
-		else
-			vars->map.map[vars->player.y / 64][vars->player.x / 64] = '0';
-		check_exit(vars, 4);
-		vars->player.x += 64;
-		vars->player.nb++;
-		ft_putmoves(vars);
-		check_collectible(vars);
-	}
-}
-
-void	move_left(t_vars *vars)
-{
-	if (vars->player.x - 64 >= 0
-		&& !check_collision(vars, 3))
-	{
-		if (check_stand_exit(vars))
-			vars->map.map[vars->player.y / 64][vars->player.x / 64] = 'E';
-		else
-			vars->map.map[vars->player.y / 64][vars->player.x / 64] = '0';
-		check_exit(vars, 3);
-		vars->player.x -= 64;
-		vars->player.nb++;
-		ft_putmoves(vars);
-		check_collectible(vars);
-	}
-}
-
 void	move_up(t_vars *vars)
 {
 	if (vars->player.y - 64 >= 0
-		&& !check_collision(vars, 1))
+		&& !check_collision(vars, 1) && !check_black_hole(vars, 1))
 	{
 		if (check_stand_exit(vars))
 			vars->map.map[vars->player.y / 64][vars->player.x / 64] = 'E';
@@ -76,15 +42,15 @@ void	move_up(t_vars *vars)
 		check_exit(vars, 1);
 		vars->player.y -= 64;
 		vars->player.nb++;
-		ft_putmoves(vars);
 		check_collectible(vars);
+		vars->player.nb1 *= -1;
 	}
 }
 
 void	move_down(t_vars *vars)
 {
 	if (vars->player.y + 64 <= vars->screen.height - vars->player.height
-		&& !check_collision(vars, 2))
+		&& !check_collision(vars, 2) && !check_black_hole(vars, 2))
 	{
 		if (check_stand_exit(vars))
 			vars->map.map[vars->player.y / 64][vars->player.x / 64] = 'E';
@@ -93,7 +59,41 @@ void	move_down(t_vars *vars)
 		check_exit(vars, 2);
 		vars->player.y += 64;
 		vars->player.nb++;
-		ft_putmoves(vars);
 		check_collectible(vars);
+		vars->player.nb1 *= -1;
+	}
+}
+
+void	move_right(t_vars *vars)
+{
+	if (vars->player.x + 64 <= vars->screen.width - vars->player.width
+		&& !check_collision(vars, 4) && !check_black_hole(vars, 4))
+	{
+		if (check_stand_exit(vars))
+			vars->map.map[vars->player.y / 64][vars->player.x / 64] = 'E';
+		else
+			vars->map.map[vars->player.y / 64][vars->player.x / 64] = '0';
+		check_exit(vars, 4);
+		vars->player.x += 64;
+		vars->player.nb++;
+		check_collectible(vars);
+		vars->player.nb1 *= -1;
+	}
+}
+
+void	move_left(t_vars *vars)
+{
+	if (vars->player.x - 64 >= 0
+		&& !check_collision(vars, 3) && !check_black_hole(vars, 3))
+	{
+		if (check_stand_exit(vars))
+			vars->map.map[vars->player.y / 64][vars->player.x / 64] = 'E';
+		else
+			vars->map.map[vars->player.y / 64][vars->player.x / 64] = '0';
+		check_exit(vars, 3);
+		vars->player.x -= 64;
+		vars->player.nb++;
+		check_collectible(vars);
+		vars->player.nb1 *= -1;
 	}
 }
