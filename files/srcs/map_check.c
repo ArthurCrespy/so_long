700419d093e61a349daccfa17f6422b48c6787bb
CRESPY Arthur <arthur@crespy.eu>
2023-02-23 13:24:49 +0100

    Move map path test in last avoiding leaks when map open

--- files/srcs/map_check.c
+++ files/srcs/map_check.c
@@ -6,10 +6,9 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 21:15:41 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/22 21:15:42 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/22 23:14:29 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "../includes/so_long.h"
 
@@ -17,8 +16,8 @@ void	check_map(t_vars *vars)
 {
 	check_map_shape(vars);
 	check_map_walls(vars);
-	check_map_path(vars);
 	check_map_letters(vars);
 	check_map_player(vars);
 	check_map_exit(vars);
+	check_map_path(vars);
 }
