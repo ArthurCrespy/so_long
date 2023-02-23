8742ecac57660a765a1913b6d37cd03ec38dd022
CRESPY Arthur <arthur@crespy.eu>
2023-02-23 13:22:21 +0100

    Add BH test and fix Norminette for exit test

--- files/srcs/game_exit.c
+++ files/srcs/game_exit.c
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 15:55:34 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/22 21:15:26 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/23 12:57:32 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,15 +42,30 @@ void	check_exit(t_vars *vars, int direction)
 		if (direction == 4 && vars->map.map[i_col][i_row + 1] == 'E')
 			mlx_exit(vars, "You win!\n", 0);
 	}
-	else
-	{
-		if (direction == 1 && vars->map.map[i_col - 1][i_row] == 'E')
-			ft_putstr("You must collect all the coins!\n");
-		if (direction == 2 && vars->map.map[i_col + 1][i_row] == 'E')
-			ft_putstr("You must collect all the coins!\n");
-		if (direction == 3 && vars->map.map[i_col][i_row - 1] == 'E')
-			ft_putstr("You must collect all the coins!\n");
-		if (direction == 4 && vars->map.map[i_col][i_row + 1] == 'E')
-			ft_putstr("You must collect all the coins!\n");
-	}
+	if (direction == 1 && vars->map.map[i_col - 1][i_row] == 'E')
+		ft_putstr("You must collect all the coins!\n");
+	if (direction == 2 && vars->map.map[i_col + 1][i_row] == 'E')
+		ft_putstr("You must collect all the coins!\n");
+	if (direction == 3 && vars->map.map[i_col][i_row - 1] == 'E')
+		ft_putstr("You must collect all the coins!\n");
+	if (direction == 4 && vars->map.map[i_col][i_row + 1] == 'E')
+		ft_putstr("You must collect all the coins!\n");
+}
+
+int	check_black_hole(t_vars *vars, int direction)
+{
+	int	i_col;
+	int	i_row;
+
+	i_col = vars->player.y / 64;
+	i_row = vars->player.x / 64;
+	if (direction == 1 && vars->map.map[i_col - 1][i_row] == 'X')
+		mlx_exit(vars, "You loose!\n", 0);
+	if (direction == 2 && vars->map.map[i_col + 1][i_row] == 'X')
+		mlx_exit(vars, "You loose!\n", 0);
+	if (direction == 3 && vars->map.map[i_col][i_row - 1] == 'X')
+		mlx_exit(vars, "You loose!\n", 0);
+	if (direction == 4 && vars->map.map[i_col][i_row + 1] == 'X')
+		mlx_exit(vars, "You loose!\n", 0);
+	return (0);
 }
