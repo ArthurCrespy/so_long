895c89bd739a891fa4219eef51eab3706d04df45
CRESPY Arthur <arthur@crespy.eu>
2023-02-27 16:57:04 +0100

    Make read_map exit if void line or void file detected and split for Norminette

--- files/srcs/map_open_file.c
+++ files/srcs/map_open_file.c
@@ -6,11 +6,18 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 21:16:03 by acrespy           #+#    #+#             */
-/*   Updated: 2023/02/22 21:16:06 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/02/27 15:01:17 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+void	ft_exit_map(char *gnl, char *join)
+{
+	free(gnl);
+	free(join);
+	ft_exit("Error: Map must not have void lines\n", 1);
+}
 
 void	save_map(t_vars *vars, char *map)
 {
@@ -19,6 +26,7 @@ void	save_map(t_vars *vars, char *map)
 	vars->map.exit_map = ft_split(map, '\n');
 	vars->map.height = ft_strlen(map) / ft_strlen(vars->map.map[0]) * 64;
 	vars->map.width = ft_strlen(vars->map.map[0]) * 64;
+	free(map);
 }
 
 void	read_map(t_vars *vars, char *path)
@@ -28,24 +36,24 @@ void	read_map(t_vars *vars, char *path)
 	char	*gnl;
 	char	*join;
 
-	join = ft_calloc(sizeof (char), 1);
+	join = NULL;
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
-	{
-		free(join);
 		ft_exit("Error: Open failed\n", 1);
-	}
 	while (1)
 	{
 		gnl = get_next_line(fd);
 		if (!gnl)
 			break ;
+		if (ft_strlen(gnl) == 1)
+			ft_exit_map(gnl, join);
 		tmp = join;
 		join = ft_strjoin(tmp, gnl);
 		free(tmp);
 		free(gnl);
 	}
+	if (ft_strlen(join) == 0)
+		ft_exit("Error: Map must not be empty\n", 1);
 	save_map(vars, join);
-	free(join);
 	close(fd);
 }
